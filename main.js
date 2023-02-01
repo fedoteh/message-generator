3e01cf23833ad0ba8a4c4714541110512037282f
fedoteh <fedebenavidez@live.com>
2023-02-01 00:34:36 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstStatement = [
+    'worst', 'best', 'most interesting', 'least interesting', 'real deal', 'best thing', 'most dissappointing', 'most anticipated', 'funnier', 'saddest'
+];
+const secondStatement = [
+    'videogame', 'experience', 'piece of art', 'art representation', 'piece of crap'
+];
+const thirdStatement = [
+    'Uncharted', 'God of War', 'God of War: Ragnarok', 'Spider-man', 'The Last Of Us: Part 1', 'The Last Of Us: Part 2', 'The Last Of Us: Part 3', 'Rocket League', 'Rainbow Six: Siege'
+];
+
